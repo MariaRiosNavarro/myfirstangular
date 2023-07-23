4fdad62e32bb0a91aeda00759d4dcb2fa0d5584c
Maria Rios Navarro <propscode254@gmail.com>
2023-07-23 17:12:15 +0200

    cat card created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CatCardComponent } from './cat-card/cat-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CatCardComponent
   ],
   imports: [
     BrowserModule,
